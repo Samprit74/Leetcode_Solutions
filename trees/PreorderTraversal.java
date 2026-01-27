e096921be82a4b96886d2869c2f50c66877f00f6
Samprit <143952230+Samprit74@users.noreply.github.com>
2026-01-27 23:28:21 +0530

    Add preorder traversal method for binary tree

---
+++ trees/PreorderTraversal.java
@@ -0,0 +1,16 @@
+// Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        dfs(root, res);
+        return res;
+    }
+
+    void dfs(TreeNode root, List<Integer> res) {
+        if (root == null) return;
+        res.add(root.val);
+        dfs(root.left, res);
+        dfs(root.right, res);
+    }
+}
