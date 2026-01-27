8bb557c05635f63198fbc062e673aa0131202914
Samprit <143952230+Samprit74@users.noreply.github.com>
2026-01-27 23:30:37 +0530

    Clarify purpose of InvertTree.java
    Remove unnecessary comment and clarify purpose.

---
+++ trees/InvertTree.java
@@ -0,0 +1,12 @@
+// Given the root of a binary tree, invert the tree.
+
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        root.left = right;
+        root.right = left;
+        return root;
+    }
+}
