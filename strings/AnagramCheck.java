28cfaef5966f09bf945e968eb39ea046dd5fb60e
Samprit <143952230+Samprit74@users.noreply.github.com>
2026-01-27 23:36:05 +0530

    Add anagram check function in AnagramCheck.java

---
+++ strings/AnagramCheck.java
@@ -0,0 +1,13 @@
+// Given two strings s and t, return true if t is an anagram of s.
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+        int[] count = new int[26];
+        for (char c : s.toCharArray()) count[c - 'a']++;
+        for (char c : t.toCharArray()) {
+            if (--count[c - 'a'] < 0) return false;
+        }
+        return true;
+    }
+}
