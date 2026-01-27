048900248e00c7b7698e6298d7a129d31d7989d2
Samprit <143952230+Samprit74@users.noreply.github.com>
2026-01-27 23:34:29 +0530

    Create ReverseString.java

---
+++ strings/ReverseString.java
@@ -0,0 +1,16 @@
+// Given a string s, return the reversed string.
+
+class Solution {
+    public String reverseString(String s) {
+        char[] arr = s.toCharArray();
+        int left = 0, right = arr.length - 1;
+        while (left < right) {
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+        return new String(arr);
+    }
+}
