c4f6dc923fc0761a469baee267e6a8b83a430373
Samprit <143952230+Samprit74@users.noreply.github.com>
2026-01-27 23:32:14 +0530

    Add comment for countNodes method in CountNodes.java

---
+++ trees/CountNodes.java
@@ -0,0 +1,8 @@
+// Given the root of a binary tree, return the total number of nodes.
+
+class Solution {
+    public int countNodes(TreeNode root) {
+        if (root == null) return 0;
+        return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+}
