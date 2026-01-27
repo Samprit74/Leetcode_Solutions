37214ac50f891221db31ce2d839ab600f4b6fd5c
Samprit <143952230+Samprit74@users.noreply.github.com>
2026-01-27 23:24:17 +0530

    Add MaxSubArray.java for largest sum subarray

---
+++ Leetcode_150/Two_Sum/MaxSubArray.java
@@ -0,0 +1,12 @@
+// Given an integer array nums, find the subarray with the largest sum.
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int max = nums[0], curr = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            curr = Math.max(nums[i], curr + nums[i]);
+            max = Math.max(max, curr);
+        }
+        return max;
+    }
+}
