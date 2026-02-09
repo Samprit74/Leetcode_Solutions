8e125349881335946903140e14c637e238844917
Samprit <143952230+Samprit74@users.noreply.github.com>
2026-02-09 11:47:57 +0530

    Add solution for finding pivot index
    Implement pivot index calculation to find index where left sum equals right sum.

---
+++ Leetcode_150/Arrays/Pivot_Index.java
@@ -0,0 +1,21 @@
+/*
+LeetCode 724 - Find Pivot Index
+
+Return the index where left sum equals right sum.
+*/
+
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int total = 0;
+        for (int n : nums) total += n;
+
+        int left = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (left == total - left - nums[i]) return i;
+            left += nums[i];
+        }
+
+        return -1;
+    }
+}
