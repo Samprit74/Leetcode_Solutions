1164d3b4b47cc4460615376d3fc3186b918e24b6
Samprit <143952230+Samprit74@users.noreply.github.com>
2026-02-09 11:52:15 +0530

    Create Max_Consecutive_ones_iii.java

---
+++ Leetcode_150/Sliding_Window/Max_Consecutive_ones_iii.java
@@ -0,0 +1,25 @@
+/*
+LeetCode 1004 - Max Consecutive Ones III
+
+Return the longest subarray containing
+at most k zeros using sliding window.
+*/
+
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int left = 0, zeros = 0, max = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] == 0) zeros++;
+
+            while (zeros > k) {
+                if (nums[left] == 0) zeros--;
+                left++;
+            }
+
+            max = Math.max(max, right - left + 1);
+        }
+
+        return max;
+    }
+}
