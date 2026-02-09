5e1f5b2701fbd43f5abc7d3a8c55422433da4851
Samprit <143952230+Samprit74@users.noreply.github.com>
2026-02-09 11:51:36 +0530

    Create Max_Avg_Subarr_1.java

---
+++ Leetcode_150/Sliding_Window/Max_Avg_Subarr_1.java
@@ -0,0 +1,23 @@
+/*
+LeetCode 643 - Maximum Average Subarray I
+
+Find the maximum average of any subarray
+of size k using sliding window.
+*/
+
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        int sum = 0;
+
+        for (int i = 0; i < k; i++) sum += nums[i];
+
+        int max = sum;
+
+        for (int i = k; i < nums.length; i++) {
+            sum += nums[i] - nums[i - k];
+            max = Math.max(max, sum);
+        }
+
+        return (double) max / k;
+    }
+}
